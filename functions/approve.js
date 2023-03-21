da41109e2f833c5cb8e77670b05da8490bb29ff2
Britton Mathews <brittonmathews@gmail.com>
2023-03-21 13:10:13 -0700

    Adding in first code

--- functions/approve.js
+++ functions/approve.js
@@ -1,15 +1,19 @@
 exports.handler = async (event, context) => {
     console.log(event);
-    //const request = JSON.parse(event.body);
+    let request = {};
 
-    // if (request.challenge) {
-    //     return {
-    //         statusCode: 200,
-    //         body: request.challenge
-    //     }
-    // }
+
+    if (request.challenge) {
+        return {
+            statusCode: 200,
+            body: request.challenge
+        }
+    }
     return {
         statusCode: 200,
-        body: event
+        body: {
+            body: event.body,
+            params: event.queryStringParameters,
+        }
     }
 }
