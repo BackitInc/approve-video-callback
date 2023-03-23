b4cfb6786f266a4f3041f93ffa09a803b6b19d94
Britton Mathews <brittonmathews@gmail.com>
2023-03-23 11:51:49 -0700

    Add in debugging moar 22

--- functions/approve.js
+++ functions/approve.js
@@ -16,6 +16,7 @@ exports.handler = async (event, context) => {
     if(event.body && /^payload=+/.test(event.body)) {
         const payload = JSON.parse(querystring.parse(event.body).payload);
         const action = JSON.parse(payload.actions[0].value);
+
         const message = Object.assign(payload.message, {
             replace_original: 'true',
             blocks: [
@@ -24,41 +25,26 @@ exports.handler = async (event, context) => {
                 {
                     type: 'section',
                     text: {
-                        type: 'text',
-                        text: 'Some other text',
+                        type: 'mrkdwn',
+                        text: '*Video Approved*',
                         verbatim: false
                     }
                 },
-                // {
-                //     type: 'mrkdwn',
-                //     text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`
-                // }
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`,
+                        verbatim: false
+                    }
+                },
             ]
         });
-        console.log('ACTION');
-        console.log(action);
-        console.log('PAYLOAD');
-        console.log(payload);
         // await axios.post(action.url, {
         //     file: action.file,
         //     hash: action.hash,
         // });
-        console.log('Finished posting');
-        // message.blocks[2] = {
-        //     type: 'mrkdwn',
-        //     text: '*Video Approved*'
-        // };
-        // message.blocks[3] = {
-        //     type: 'mrkdwn',
-        //     text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`
-        // };
-        console.log('Unmodified message');
-        console.log(JSON.stringify(payload.message));
-        console.log('Message modified');
-        console.log(message);
-        console.log('JSON STRINGIFYING');
-        console.log(JSON.stringify(message));
-        //await axios.post(payload.response_url, message);
+        await axios.post(payload.response_url, message);
     }
     return {
         statusCode: 200,
