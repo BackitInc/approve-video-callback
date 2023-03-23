2d0e18801d961872098d70e29543e573902f3634
Britton Mathews <brittonmathews@gmail.com>
2023-03-23 10:12:19 -0700

    Add in debugging moar 9

--- functions/approve.js
+++ functions/approve.js
@@ -23,13 +23,26 @@ exports.handler = async (event, context) => {
             hash: action.hash,
         });
         console.log('Finished posting');
-        const message = Object.assign({}, payload.message);
+        const message = Object.assign(payload.message, {
+            blocks: [
+                ...payload.message.blocks[0],
+                ...payload.message.blocks[1],
+                {
+                    type: 'mrkdwn',
+                    text: '*Video Approved*'
+                },
+                {
+                    type: 'mrkdwn',
+                    text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`
+                }
+            ]
+        });
         console.log('The message');
         console.log(message);
-        message.blocks[2] = {
-            type: 'mrkdwn',
-            text: '*Video Approved*'
-        };
+        // message.blocks[2] = {
+        //     type: 'mrkdwn',
+        //     text: '*Video Approved*'
+        // };
         // message.blocks[3] = {
         //     type: 'mrkdwn',
         //     text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`
