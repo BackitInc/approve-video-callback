25f12493815f1465611fddc0eb3183d6246e8b31
Britton Mathews <brittonmathews@gmail.com>
2023-03-22 21:13:02 -0700

    Handle approval process

--- functions/approve.js
+++ functions/approve.js
@@ -1,5 +1,6 @@
 const querystring = require('querystring');
 const axios = require('axios');
+const { DateTime } = require('luxon');
 
 exports.handler = async (event, context) => {
     console.log(event);
@@ -13,41 +14,22 @@ exports.handler = async (event, context) => {
     }
 
     if(event.body && /^payload=+/.test(event.body)) {
-        const payload = JSON.parse(querystring.parse(event.body).payload)
-        const date = new Date();
-        await axios.post(payload.response_url, {
-            "replace_original": "true",
-            text: 'New comic book alert!',
-            attachments: [
-                {
-                    title: 'The Further Adventures of Slackbot',
-                    fields: [
-                        {
-                            title: 'Volume',
-                            value: '1',
-                            short: true,
-                        },
-                        {
-                            title: 'Issue',
-                            value: '3',
-                            short: true,
-                        },
-                    ],
-                    author_name: 'Stanford S. Strickland',
-                    author_icon:
-                        'http://a.slack-edge.com/7f18https://a.slack-edge.com/80588/img/api/homepage_custom_integrations-2x.png',
-                    image_url: 'http://i.imgur.com/OJkaVOI.jpg?1',
-                },
-                {
-                    title: 'Synopsis',
-                    text: 'After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy...',
-                },
-                {
-                    title: `${payload.user.name} approved this video`,
-                    text: `Approved at ${date.toTimeString()}`
-                },
-            ]
+        const payload = JSON.parse(querystring.parse(event.body).payload);
+        const action = JSON.parse(payload.actions[0].value);
+        await axios.post(action.url, {
+            file: action.file,
+            hash: action.hash,
         });
+        const message = payload.message;
+        message.blocks[2] = {
+            type: 'mrkdwn',
+            text: '*Video Approved*'
+        };
+        message.blocks[3] = {
+            type: 'mrkdwn',
+            text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`
+        };
+        await axios.post(payload.response_url, message);
     }
     return {
         statusCode: 200,
