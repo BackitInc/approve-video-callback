a8569edb0c2d7dfd2d8d321f14ce05da793d961a
Britton Mathews <brittonmathews@gmail.com>
2023-03-23 18:50:16 -0700

    Add in ability to deny

--- functions/approve.js
+++ functions/approve.js
@@ -16,28 +16,44 @@ exports.handler = async (event, context) => {
     if(event.body && /^payload=+/.test(event.body)) {
         const payload = JSON.parse(querystring.parse(event.body).payload);
         const action = JSON.parse(payload.actions[0].value);
+        const report = action.action === 'approve' ? [{
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: '*Video Approved*',
+                    verbatim: false
+                }
+            },
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`,
+                    verbatim: false
+                }
+            }] : [{
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: '*Video Denied*',
+                    verbatim: false
+                }
+            },
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `:x: ${payload.user.name} denied this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`,
+                    verbatim: false
+                }
+            }];
 
         const message = Object.assign(payload.message, {
             replace_original: 'true',
             blocks: [
                 payload.message.blocks[0],
                 payload.message.blocks[1],
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: '*Video Approved*',
-                        verbatim: false
-                    }
-                },
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: `:white_check_mark: ${payload.user.name} approved this video on ${DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}`,
-                        verbatim: false
-                    }
-                },
+                ...report,
             ]
         });
         await axios.post(action.url, {
